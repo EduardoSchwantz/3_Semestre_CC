ae2bafc5a62e14fd2f20eca0dd7a2a07e666beca
Eduardo Schwantz <eduardo_schwantz@hotmail.com>
2022-05-29 17:54:07 -0300

    sonting completo

--- Sorting.c
+++ Sorting.c
@@ -1,58 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <windows.h>
 #include <time.h>
+void preenche_vetor(int *vetor, int tam);
 void imprimir(int *vetor, int tam);
 void selection_sort(int *vetor, int tam);
 void insertion_sort(int *vetor, int tam);
 void quick_sort(int *vetor, int pos_esq, int pos_dir);
-int particiona(int *vetor, int inicio, int fim);
+void merge_sort(int *vetor, int inicio, int fim);
+int particiona_quick_sort(int *vetor, int inicio, int fim);
+void particiona_merge_sort(int *vetor, int inicio, int meio, int fim);
 
 int main()
 {
     SetConsoleOutputCP(65001);
-    int tam = 0, opcao, sair, i, valor, *vetor;
-
+    int tam, opcao, sair, *vetor;
     srand(time(NULL));
 
     do
     {
         printf("\n\n\n             Selecione uma opção:             \n");
-        printf(" 1 - Criar vetor aleatório.\n");
+        printf(" 1 - Criar vetor e preencher aleatóriamente o vetor.\n");
         printf(" 2 - Ordenação SELECTION SORT. \n");
         printf(" 3 - Ordenação INSERTION SORT.\n");
         printf(" 4 - Ordenação QUICK SORT.\n");
-        printf(" 5 - Imprimir vetor.\n");
-        printf(" 6 - Limpar vetor.\n");
-        printf(" 7 - Sair.\n");
+        printf(" 5 - Ordenação MERGE SORT.\n");
+        printf(" 6 - Imprimir vetor.\n");
+        printf(" 7 - Limpar vetor.\n");
+        printf(" 0 - Sair.\n");
         printf("\n             Digite uma opção:             \n");
         scanf("%d", &opcao);
         getchar();
         system("clear||cls");
-
         switch (opcao)
         {
         case 1:
             printf("\nDigite o tamanho do vetor: ");
             scanf("%d", &tam);
-            vetor = malloc(tam * sizeof(int));
-            for (i = 0; i < tam; i++)
-            {
-                valor = 0;
-                valor = rand() % 999;
-                vetor[i] = valor;
-            }
+            vetor = (int *)malloc(tam * sizeof(int));
+            preenche_vetor(vetor, tam);
             break;
 
         case 2:
-            selection_sort(vetor, tam);
+            selection_sort(vetor, tam - 1);
             printf("\nVetor ordenado utilizando SELECTION SORT\n");
             imprimir(vetor, tam);
             break;
 
         case 3:
-            insertion_sort(vetor, tam);
+            insertion_sort(vetor, tam - 1);
             printf("\nVetor ordenado utilizando INSERTION SORT\n");
             imprimir(vetor, tam);
             break;
@@ -63,14 +61,20 @@ int main()
             break;
 
         case 5:
+            merge_sort(vetor, 0, tam - 1);
+            printf("\nVetor ordenado utilizando MERGE SORT\n");
+            imprimir(vetor, tam);
+            break;
+        case 6:
             imprimir(vetor, tam);
             break;
 
-        case 6:
+        case 7:
             free(vetor);
+            tam = 0;
             break;
 
-        case 7:
+        case 0:
             sair = 0;
             break;
 
@@ -83,6 +87,15 @@ int main()
     free(vetor);
     return 0;
 }
+void preenche_vetor(int *vetor, int tam)
+{
+    int i;
+    for (i = 0; i < tam; i++)
+    {
+        vetor[i] = rand() % 999;
+    }
+}
+
 void imprimir(int *vetor, int tam)
 {
     printf("\n             Imprimindo Vetor:             \n");
@@ -96,7 +109,7 @@ void imprimir(int *vetor, int tam)
 void selection_sort(int *vetor, int tam)
 {
     int i, j, menor, troca;
-    for (i = 0; i < tam - 1; i++)
+    for (i = 0; i < tam; i++)
     {
         menor = i;
         for (j = i + 1; j < tam; j++)
@@ -115,7 +128,7 @@ void selection_sort(int *vetor, int tam)
 void insertion_sort(int *vetor, int tam)
 {
     int i, j, aux;
-    for (i = 1; i < tam; i++)
+    for (i = 0; i < tam; i++)
     {
         aux = vetor[i];
         for (j = i; (j > 0) && (aux < vetor[j - 1]); j--)
@@ -131,13 +144,13 @@ void quick_sort(int *vetor, int inicio, int fim)
     int pivo;
     if (fim > inicio)
     {
-        pivo = particiona(vetor, inicio, fim);
+        pivo = particiona_quick_sort(vetor, inicio, fim);
         quick_sort(vetor, inicio, pivo - 1);
         quick_sort(vetor, pivo + 1, fim);
     }
 }
 
-int particiona(int *vetor, int inicio, int fim)
+int particiona_quick_sort(int *vetor, int inicio, int fim)
 {
     int esq, dir, pivo, aux;
     esq = inicio;
@@ -161,4 +174,55 @@ int particiona(int *vetor, int inicio, int fim)
     vetor[inicio] = vetor[dir];
     vetor[dir] = pivo;
     return dir;
+}
+
+void merge_sort(int *vetor, int inicio, int fim)
+{
+    int meio;
+    if (inicio < fim)
+    {
+        meio = floor(inicio + fim) / 2;
+        merge_sort(vetor, inicio, meio);
+        merge_sort(vetor, meio + 1, fim);
+        particiona_merge_sort(vetor, inicio, meio, fim);
+    }
+}
+
+void particiona_merge_sort(int *vetor, int inicio, int meio, int fim)
+{
+    int *temp, p1, p2, tamanho, i, j, k;
+    int fim1 = 0, fim2 = 0;
+    tamanho = fim - inicio + 1;
+    p1 = inicio;
+    p2 = meio + 1;
+    temp = (int *)malloc(tamanho * sizeof(int));
+
+    if (temp != NULL)
+    {
+        for (i = 0; i < tamanho; i++)
+        {
+            if (!fim1 && !fim2)
+            {
+                if (vetor[p1] < vetor[p2])
+                    temp[i] = vetor[p1++];
+                else
+                    temp[i] = vetor[p2++];
+
+                if (p1 > meio)
+                    fim1 = 1;
+                if (p2 > fim)
+                    fim2 = 1;
+            }
+            else
+            {
+                if (!fim1)
+                    temp[i] = vetor[p1++];
+                else
+                    temp[i] = vetor[p2++];
+            }
+        }
+        for (j = 0, k = inicio; j < tamanho; j++, k++)
+            vetor[k] = temp[j];
+    }
+    free(temp);
 }
